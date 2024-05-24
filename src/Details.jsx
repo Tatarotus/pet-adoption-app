192b230ebfa0440a0b5a67de02b88e3570bdd9d7
Samuel Baruch <shamuelbaruch@gmail.com>
2024-05-24 16:02:19 -0300

    Lesson 21

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,10 @@
+import { useParams } from 'react-router-dom'
+
 const Details = () => {
+  const { id } = useParams()
   return (
     <div>
-      <h3>Details</h3>
+      <h3>Details {id}</h3>
     </div>
   )
 }
