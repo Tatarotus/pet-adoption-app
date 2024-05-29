e8376a669167d23602f8b13d579b422b79e24f56
Samuel Baruch <shamuelbaruch@gmail.com>
2024-05-29 16:51:28 -0300

    Lesson 23

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,29 @@
 import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import loadingGif from './assets/spinner.gif'
+import fetchPet from './fetchPet'
 
 const Details = () => {
   const { id } = useParams()
+  const results = useQuery(['details', id], fetchPet)
+  if (results.isLoading) {
+    return (
+      <div className='loading-pane'>
+        <img className='loader' alt='loader' src={loadingGif} />
+      </div>
+    )
+  }
+  const pet = results.data.pets[0]
   return (
-    <div>
-      <h3>Details {id}</h3>
+    <div className='details'>
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
     </div>
   )
 }
