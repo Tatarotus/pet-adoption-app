eaea1e47676a21ae5d00407ff0a0a2d2679cb96c
Samuel Baruch <shamuelbaruch@gmail.com>
2024-05-31 14:31:27 -0300

    Lesson 30

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import ErrorBoundary from './ErrorBoundary'
 import loadingGif from './assets/spinner.gif'
 import fetchPet from './fetchPet'
 import Carousel from './Carousel'
+import Modal from './Modal'
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false)
   const { id } = useParams()
   const results = useQuery(['details', id], fetchPet)
   if (results.isLoading) {
@@ -24,8 +27,29 @@ const Details = () => {
         <h2>
           {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
         </h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <h1>Would you like to adopt {pet.name}?</h1>
+            <div className='buttons'>
+              <button
+                onClick={() => {
+                  setShowModal(false)
+                }}
+              >
+                Yes
+              </button>
+              <button
+                onClick={() => {
+                  setShowModal(false)
+                }}
+              >
+                No
+              </button>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   )
